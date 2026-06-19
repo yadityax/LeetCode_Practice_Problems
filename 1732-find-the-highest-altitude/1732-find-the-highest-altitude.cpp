d81b0dc8a817c8f8e529b597fd30f43f970a93ec
Aditya Arun Kumar Yadav <91359943+yadityax@users.noreply.github.com>
2026-06-19 13:16:02 +0530

    Time: 0 ms (100%), Space: 10.8 MB (44.68%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -2,15 +2,33 @@ class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
 
-        vector<int> netgain(gain.size() + 1);
-
-        netgain[0] = 0;
+        int altitude = 0;
+        int maxAltitude = 0;
 
         for (int i = 0; i < gain.size(); i++) {
 
-            netgain[i + 1] = netgain[i] + gain[i];
+            altitude += gain[i];
+
+            maxAltitude = max(maxAltitude, altitude);
         }
 
-        return *max_element(netgain.begin(), netgain.end());
+        return maxAltitude;
     }
 };
+
+// class Solution {
+// public:
+//     int largestAltitude(vector<int>& gain) {
+
+//         vector<int> netgain(gain.size() + 1);
+
+//         netgain[0] = 0;
+
+//         for (int i = 0; i < gain.size(); i++) {
+
+//             netgain[i + 1] = netgain[i] + gain[i];
+//         }
+
+//         return *max_element(netgain.begin(), netgain.end());
+//     }
+// };
