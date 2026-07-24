97a734ab1addd19f50fadb5fb0bac8b66ba9e223
Aditya Arun Kumar Yadav <91359943+yadityax@users.noreply.github.com>
2026-07-24 11:25:47 +0530

    Time: 0 ms (100%), Space: 31.2 MB (82.16%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0, mid, end = nums.size()-1;
+        while(start<=end){
+            mid = (start + end) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                start = mid + 1;
+            else 
+                end = mid - 1;
+        }
+        return -1;
+    }
+};
