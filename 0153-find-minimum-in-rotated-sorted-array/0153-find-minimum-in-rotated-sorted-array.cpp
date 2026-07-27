501cfd268abb61a9b972eb303e73e088163eb65a
Aditya Arun Kumar Yadav <91359943+yadityax@users.noreply.github.com>
2026-07-27 12:05:26 +0530

    Time: 0 ms (100%), Space: 14.2 MB (12.15%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,8 +4,10 @@ public:
         int ans = nums[0], start = 0, mid, end = nums.size() - 1;
         while(start <= end){
             mid = start + (end - start) / 2;
+            // Left side sorted array
             if(nums[mid] >= nums[0])
                 start = mid + 1;
+            // Right side sorted array
             else{
                 ans = nums[mid];
                 end = mid - 1;
