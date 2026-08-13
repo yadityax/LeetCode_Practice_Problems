0ccfa3a3c3e1b6001c5c5acbd4d23a34f87e36a0
Aditya Arun Kumar Yadav <91359943+yadityax@users.noreply.github.com>
2026-08-14 00:37:27 +0530

    Time: 0 ms (100%), Space: 17.3 MB (99.92%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -10,13 +10,13 @@ public:
     // }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int>dp(n+1,-1);
-        if(n<=1) return 0;
-        dp[0]=0;
-        dp[1]=0;
+        int curr, prev=0, prev1=0;
+
         for(int i=2;i<=n;i++){
-        dp[i] = min(cost[i-1]+dp[i-1], cost[i-2]+dp[i-2]);
+            curr = min(cost[i-1]+prev, cost[i-2]+prev1);
+            prev1 = prev;
+            prev = curr;
         }
-        return dp[n];
+        return prev;
     }
 };
