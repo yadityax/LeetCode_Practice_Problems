1279396ae95bd8d340a216c829d62b184f3f42b0
Aditya Arun Kumar Yadav <91359943+yadityax@users.noreply.github.com>
2026-08-15 17:58:02 +0530

    Time: 39 ms (72.07%), Space: 45.4 MB (70.85%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int total = 0;
+
+        unordered_map<int,int>m;
+        int PrefixSum=0;
+        m[0]=1;
+
+        for(int i=0;i<nums.size();i++){
+            PrefixSum +=nums[i];
+            if(m.count(PrefixSum-k)){
+                total+=m[PrefixSum-k];
+                m[PrefixSum]++;
+            }
+            else
+            m[PrefixSum]++;
+        }
+        return total;
+    }
+};
