3a235eb96c6fcc36dd30355ba3927ebf057971e5
Aditya Arun Kumar Yadav <91359943+yadityax@users.noreply.github.com>
2026-09-05 17:31:58 +0530

    Time: 0 ms (100%), Space: 8.6 MB (29.79%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char> st1;
+        stack<char> st2;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] >= 'a' && s[i] <= 'z') {
+                st1.push(s[i]);
+            }
+            else {
+                if (!st1.empty()) {
+                    st1.pop();
+                }
+            }
+        }
+
+        for (int i = 0; i < t.size(); i++) {
+            if (t[i] >= 'a' && t[i] <= 'z') {
+                st2.push(t[i]);
+            }
+            else {
+                if (!st2.empty()) {
+                    st2.pop();
+                }
+            }
+        }
+
+        if (st1 == st2) {
+            return true;
+        }
+
+        return false;
+    }
+};
