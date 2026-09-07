bccc9fac5b0ee6616c5cbe30e535231df6778ca7
Aditya Arun Kumar Yadav <91359943+yadityax@users.noreply.github.com>
2026-09-07 19:30:54 +0530

    Time: 15 ms (86.1%), Space: 81.4 MB (72.21%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        // Next smallest left
-        vector<int> Left(heights.size());
-        // Next smallest right
-        vector<int> Right(heights.size());
+        int ans = 0;
+        int index;
         stack<int> st;
-        // NSR filling
-        for(int i=0;i<heights.size();i++){
-          while(!st.empty() && heights[st.top()] > heights[i]){
-            Right[st.top()] = i;
-            st.pop();
-          }  
-          st.push(i);
-        }
-        while(!st.empty()){
-            Right[st.top()] = heights.size();
-            st.pop();
-        }
-        // NSL filling
-        for(int i=heights.size()-1;i>=0;i--){
-        while(!st.empty() && heights[st.top()] > heights[i]){
-            Left[st.top()] = i;
-            st.pop();
-        }
-        st.push(i);
-        }
-        while(!st.empty()){
-            Left[st.top()] = -1;
-            st.pop();
+        int n = heights.size();
+
+        for(int i=0;i<n;i++){
+            while(!st.empty() && heights[st.top()] > heights[i]){
+                index = st.top();
+                st.pop();
+                if(!st.empty())
+                ans = max(ans, heights[index]*(i-st.top()-1));
+                else
+                ans = max(ans, heights[index]*i);
+            }
+            st.push(i);
         }
 
-        int maxx = INT_MIN;
-        for(int i=0;i<heights.size();i++){
-            maxx = max(maxx, heights[i]*(Right[i] - Left[i] - 1));
-        }
-        return maxx;
+        while(!st.empty()){
+                index = st.top();
+                st.pop();
+                if(!st.empty())
+                ans = max(ans, heights[index]*(n-st.top()-1));
+                else
+                ans = max(ans, heights[index]*n);
+            }
+            return ans;
+
+
     }
 };
